128950496fd887ddbc0394be17830b81f7940614
Dongha <dkang0602@gmail.com>
2020-11-08 02:05:13 +0900

    Practice server jsp

--- Server/servlet_practice/src/com/calc/CalcResult.java
+++ Server/servlet_practice/src/com/calc/CalcResult.java
@@ -17,22 +17,33 @@ public class CalcResult extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1. 한글처리 (UTF-8, EUC-KR)..
 		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
 		
 		// 2. 데이터 가져오기 
-		String a = request.getParameter("a");
-		String b = request.getParameter("b");
+		int a = Integer.parseInt(request.getParameter("a"));
+		int b = Integer.parseInt(request.getParameter("b"));
+		String op = request.getParameterValues("op")[0];
 		
+		String ans = "";
+		
+		if (op.equals("+")) {
+			ans = (a + b) + ""; 
+		} else if (op.equals("-")) {
+			ans = (a - b) + "";
+		} else if (op.equals("*")) {
+			ans = (a * b) + "";
+		} else if (op.equals("/")) {
+			ans = ((double)a / (double)b) + "";
+		}
 		
 		// 3. 출력
 		response.getWriter().append("<html><head><title></title></head>");
 		response.getWriter().append("<body>");
-		
-		response.getWriter().append(a + "<br>");
-		response.getWriter().append(b + "<br>");
-		
+		response.getWriter().append("<h1>테스트 프로그래밍</h1>");
+		response.getWriter().append("답 : " + ans + "입니다." );
 		
 		response.getWriter().append("</body></html>");
 	}
